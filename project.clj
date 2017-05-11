fbe1c2ca181b309d58d18a8ebeda2e67208eed23
David Frese <david.frese@active-group.de>
2017-05-11 23:43:16 +0200

    Calliope is a purely functional web application framework.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject dfrese/calliope-core "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Calliope is a purely functional web application framework."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
